cf2d39080edc2008f65bda8fe3d3c3eed7763da6
Luis Antonio <luis.developer.ac@gmail.com>
2022-12-19 21:40:37 -0500

    listen port from env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -8,4 +11,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
